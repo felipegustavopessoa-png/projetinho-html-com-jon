965d29285330e41ab120c11f57227224817bbee9
felipegustavopessoa-png <felipe.gustavo.pessoa@escola.pr.gov.br>
2026-06-30 17:02:18 -0300

    mudamo tudo

--- javas.js
+++ javas.js
@@ -1,1 +1,27 @@
+function matricular(){
+    alert("Bem-vindo à Power Gym! Nossa equipe entrará em contato.");
+}
 
+function plano(nome){
+    alert("Você escolheu o Plano " + nome + "!");
+}
+
+function contar(){
+
+    let numero = document.getElementById("numero");
+
+    let valor = 0;
+
+    let intervalo = setInterval(function(){
+
+        valor += 25;
+
+        numero.innerHTML = valor;
+
+        if(valor >= 500){
+            clearInterval(intervalo);
+        }
+
+    },40);
+
+}
